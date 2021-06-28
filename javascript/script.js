bdbae6828274eb2ac4cb087f1790e9b4e3acce65
Joao Augusto <67336838+joaoaugusto05@users.noreply.github.com>
2021-06-28 11:30:54 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,44 @@
+/*Menu animado */
+const open_btn = document.querySelector('.open-btn')
+const close_btn = document.querySelector('.close-btn')
+const nav = document.querySelectorAll('.nav')
+
+open_btn.addEventListener('click', () =>{
+    nav.forEach(nav_el => nav_el.classList.add('visible'))
+})
+
+close_btn.addEventListener('click', () =>{
+    nav.forEach(nav_el => nav_el.classList.remove('visible'))
+})
+
+
+
+
+
+/* Animação Cards */
+const target = document.querySelectorAll('[data-anime]')
+const second = target[1]
+const animation_class = 'animate'
+window.addEventListener('scroll', anime_scroll)
+
+function anime_scroll(){
+    const windowTop = window.pageYOffset + (window.innerHeight*2)/4;
+    target.forEach(function(element){
+        if(windowTop > element.offsetTop){
+            element.classList.add(animation_class)
+        }
+        else{
+            element.classList.remove(animation_class)
+        }
+        if(windowTop> document.documentElement.clientHeight ){
+            open_btn.style.color = "black";
+        }
+        else{
+            open_btn.style.color = "white";
+        }
+    
+        
+    })
+
+    
+}
